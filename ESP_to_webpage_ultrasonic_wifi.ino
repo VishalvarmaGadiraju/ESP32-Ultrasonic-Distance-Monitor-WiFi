781cf43504cecbbb4fdd56505e6b493eb40ee040
VishalvarmaGadiraju <vishalvarmagadiraju2107@gmail.com>
2026-06-15 20:56:24 +0530

    Add files via upload

---
+++ ESP_to_webpage_ultrasonic_wifi.ino
@@ -0,0 +1,122 @@
+#include <WiFi.h>
+#include <WebServer.h>
+
+const char* ssid = "ESP32_WIFI";
+const char* password = "123456789";
+
+WebServer server(80);
+
+#define TRG_pin 5
+#define ECHO_pin 18
+
+String webpage = R"rawliteral(
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ESP32 Ultrasonic Sensor</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <style>
+        body{
+            text-align:center;
+            font-family:Arial;
+            margin-top:50px;
+            background-color:#f2f2f2;
+        }
+
+        .card{
+            width:300px;
+            margin:auto;
+            padding:20px;
+            background:white;
+            border-radius:10px;
+            box-shadow:0px 0px 10px gray;
+        }
+
+        h2{
+            color:#333;
+        }
+
+        #distance{
+            font-size:40px;
+            color:blue;
+            font-weight:bold;
+        }
+    </style>
+</head>
+
+<body>
+
+<div class="card">
+    <h2>Ultrasonic Distance</h2>
+
+    <p id="distance">0 cm</p>
+</div>
+
+<script>
+setInterval(function() {
+    fetch('/distance')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('distance').innerHTML = data + " cm";
+    })
+    .catch(error => {
+        document.getElementById('distance').innerHTML = "Error";
+        console.log(error);
+    });
+}, 1000);
+</script>
+
+</body>
+</html>
+)rawliteral";
+
+void handleroot()
+{
+  server.send(200,"text/html",webpage);
+  
+}
+void handleSensor()
+{
+
+ long duration;
+ float distance;
+
+ digitalWrite(TRG_pin,LOW);
+ delayMicroseconds(10);
+
+ digitalWrite(TRG_pin,HIGH);
+ delayMicroseconds(10);
+
+ digitalWrite(TRG_pin,LOW);
+
+ duration = pulseIn(ECHO_pin,HIGH);//pulse in stores the amount of time for which echopin was high i.e signal read time from obstacle
+ distance = (duration * 0.034) / 2;// speed of sound in cm/us = 0.034
+
+
+ String reading = "distance:"+ String(distance);// using this string for changing int it is called concatenation
+
+ server.send(200,"text/plain",reading);
+}
+void setup() {
+  // put your setup code here, to run once:
+ Serial.begin(115200);
+ pinMode(TRG_pin,OUTPUT);
+ pinMode(ECHO_pin,INPUT);
+ digitalWrite(TRG_pin,LOW);
+
+    // Start Wi-Fi Access Point
+    WiFi.softAP(ssid, password);
+    Serial.print("IP adress:");
+    Serial.println(WiFi.softAPIP());
+    server.on("/", handleroot);
+    server.on("/distance", handleSensor);
+
+    server.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+server.handleClient();//handleclient is for starting the html code which is sent
+
+}
